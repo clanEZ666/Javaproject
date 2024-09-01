4be7781deba196b0d5bc046f1db97123616f48d5
Vyacheslav Dubovitskiy <slava@python.ru>
2024-09-02 01:55:34 +0400

    kodDZ homework conditions 5.6

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,38 @@ public class Main {
 
         //      1)	Объявить и инициализировать переменные всех 8 типов примитивов
 
-        byte myByte = 50;
+      /*  byte myByte = 50;
         short myShort = 50;
         int myInt = 2_500_000;
         long myLong = 5000L;
         float myFloat = 6500;
         double myDouble = 12.0;
         char myChar = 'b';
-        boolean myBoolean = true;
-        //
-        //
+        boolean myBoolean = true; */
+
+        //       2) Арифмитические операции с целыми числами
+
+      /*
+        int a = 50;
+        int b = 20;
+
+        int summa =  a + b;
+        System.out.println("Cложение: " + summa);
+
+        int difference = a - b;
+        System.out.println("Вычитание: " + difference);
+
+        int product = a * b;
+        System.out.println("Умножение: " + product);
+
+        int quotient = a / b;
+        System.out.println("Деление: " + quotient);
+
+        int remainder = a % b;
+        System.out.println("Остаток от деления: " + remainder);
+                                                                    */
+
+
 
 
     }
