05c14f93ab770b626b2133318e4c99ad819f142c
Vyacheslav Dubovitskiy <slava@python.ru>
2024-09-02 01:11:30 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //      1)	Объявить и инициализировать переменные всех 8 типов примитивов
+
+        byte myByte = 50;
+        short myShort = 50;
+        int myInt = 2_500_000;
+        long myLong = 5000L;
+        float myFloat = 6500;
+        double myDouble = 12.0;
+        char myChar = 'b';
+        boolean myBoolean = true;
+        //
+
+
+    }
+}
+
+
+
+
